9876cf8b922600e53394fddfa850066fd351be88
Joshua Ravael <jravaellnew@gmail.com>
2025-12-06 15:13:08 +0700

    feat: add JWT encryption and decryption functions

---
+++ backend/lib/jwt.ts
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const encrypt = async (payload: { id: string }, expiresIn: any) => {
+    return jwt.sign(payload, process.env.JWT_SECRET as string, {
+                algorithm: 'HS256',
+                expiresIn
+            });
+}
+
+export const decrypt = async (token: string): Promise<{ id: string }> => {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string };
+    return decoded as {id: string};
+}
