09dccd70b75c2374313af5efcac2e4db00cec9ec
Joshua Ravael <jravaellnew@gmail.com>
2025-12-12 00:50:23 +0700

    Send code when register

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -3,7 +3,9 @@ import db from "../lib/db";
 import { StatusCodes } from "http-status-codes";
 import { z } from "zod";
 import { RegisterSchema } from "../schemas/register.schema";
+import { sendVerification } from "./email.controller";
 import bcrypt from "bcryptjs";
+import { generateVerificationToken } from "../lib/generator";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
@@ -28,7 +30,52 @@ export const createUser = async (req: Request, res: Response) => {
       },
     });
 
-    res.json({ msg: "Register behasil" });
+    // Send Verification Code
+
+    const verificationCode = generateVerificationToken();
+
+    await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "POST",
+      headers: {
+        "api-key": process.env.BREVO_API_KEY as string,
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        sender: {
+          name: "JCorp",
+          email: "jravaellnew@gmail.com",
+        },
+        to: [
+          {
+            email: user.email,
+            name: user.name,
+          },
+        ],
+        subject: "JBlog Kode Verifikasi",
+        htmlContent: `Ini kode verifikasi kamu: ${verificationCode} atau melalui link: http://localhost:3000/verify-email?code=${verificationCode}`,
+      }),
+    });
+
+    await db.verificationCode.upsert({
+      update: {
+        value: verificationCode,
+        expiredAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      },
+      create: {
+        value: verificationCode,
+        expiredAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        userId: user.id,
+      },
+      where: {
+        userId: user.id,
+      },
+    });
+
+    res.json({
+      msg: "Register behasil dan Verifikasi kode terkirim",
+      userId: user.id,
+    });
   } catch (error) {
     console.error("Login error:", error);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
