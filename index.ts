84e8f555e72c5c9ed8a13a45cd88aef9bdcc9858
Joshua Ravael <jravaellnew@gmail.com>
2025-12-12 00:50:09 +0700

    add email routes

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
-import UsersRouter from "./routes/users.route";
-import AuthRouter from "./routes/auth.route";
+import UsersRoutes from "./routes/users.route";
+import AuthRoutes from "./routes/auth.route";
+import EmailRoutes from "./routes/email.route";
 import db from "./lib/db";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
@@ -23,8 +24,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
 
-app.use("/api/users/", UsersRouter);
-app.use("/api/auth/", AuthRouter);
+app.use("/api/users/", UsersRoutes);
+app.use("/api/auth/", AuthRoutes);
+app.use("/api/email/", EmailRoutes);
 
 app.listen(8000, async () => {
   console.log("Server online!");
