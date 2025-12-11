9d892265c974abde7ef59731a8cab3e3738a945c
Joshua Ravael <jravaellnew@gmail.com>
2025-12-11 20:51:23 +0700

    Refactor server setup and middleware order

--- index.ts
+++ index.ts
@@ -10,11 +10,10 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(morgan("dev"));
+app.set("trust proxy", 1);
 app.use(
   cors({
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
     origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
@@ -22,6 +21,11 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(morgan("dev"));
 
 app.use("/api/users/", UsersRouter);
 app.use("/api/auth/", AuthRouter);
+
+app.listen(8000, async () => {
+  console.log("Server online!");
+});
