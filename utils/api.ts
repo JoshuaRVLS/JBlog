c74e619576a6edc2f42734eb484850e5d679c7a4
Joshua Ravael <jravaellnew@gmail.com>
2025-12-11 20:51:03 +0700

    fix: update Axios instance baseURL to use local API endpoint

--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: process.env.BASE_URL,
-    withCredentials: true
-})
+  baseURL: "http://localhost:8000/api",
+  withCredentials: true,
+});
 
 export default AxiosInstance;
